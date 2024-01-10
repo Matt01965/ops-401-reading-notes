0c84b252b95786f255197f89a4b0015d6e505089
Matthew Earles <142598248+Matt01965@users.noreply.github.com>
2024-01-10 14:34:13 -0800

    Create ops401d10-day03.py

---
+++ ops401d10-day03.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+# Script Name:                  ops401d8-day03
+# Author:                       Matthew Earles
+# Date of latest revision:      01/10/2024
+# Purpose:                      Uptime Sensor Tool w/ Email Alert
+
+
+# import Libraries
+import os
+import datetime
+import time
+import smtplib
+from getpass import getpass
+
+# Get user input for email, password, and target IP address
+email = input("Enter your email address: ")
+# use "app password" not, normal password
+password = getpass("Enter your password: ")
+ip = input("Enter an IP address: ")
+up = "Host in active"
+down = "Host is down"
+
+# check status change
+last = 0
+ping_result = 0 
+
+# Functions 
+
+# Function to handle down alert (change from up to down)
+def send_upAlert():
+    now = datetime.datetime.now()
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+
+    # start TLS
+    s.starttls()
+
+    # Authentication
+    s.login(email, password)
+
+    message = "Your server is back up"
+
+    # send the email
+    s.sendmail("mailbot@codefellows.com", email, message)
+
+    # Close the session
+    s.quit()
+
+# Function to handle down alert (change from down to up)
+def send_downAlert():
+    now = datetime.datetime.now()
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+
+    # start TLS
+    s.starttls()
+
+    # Authentication
+    s.login(email, password)
+
+    message = "Your server is down"
+
+    # send the email
+    s.sendmail("mailbot@codefellows.com", email, message)
+
+    # Close the session
+    s.quit()
+
+# Function to check the ping status and send alerts if there is a change.
+def check_ping():
+    now = datetime.datetime.now()
+    global ping_result # starts as 0
+    global last # starts as 0
+
+    # Sends a single ping to target and puts the response into a variable
+    response = os.system("ping -c 1 " + target)
+
+    # Check the change of ping status from up to down and down to up
+
+    if ((ping_result != last) and (ping_result == up)):
+        # Change value of last
+        last = up
+        # Calls function to send email
+        send_upAlert()
+
+    elif ((ping_result != last) and (ping_result == down)):
+        
+        # Change value of last
+        last = down
+        # Calls function to send email
+        send_downAlert()
+    
+    # Ping User inputted IP address and put it into local variable "response" 
+    response = os.system("ping -c 1 " + ip)
+
+    # Evaulate the ping response
+    if response == 0:
+        ping_result = up 
+    else:
+        ping_result = down
+
+    print(str(now) + ping_result + "to " + ip)
+
+# Main 
+
+# infinite loop
+
+while True: 
+    check_ping()
+    time.sleep(2)
